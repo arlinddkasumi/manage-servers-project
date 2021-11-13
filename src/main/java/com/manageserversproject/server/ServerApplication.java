459ad3a65ff9b1d40cb4b25000cab0a45093fe25
arlinddkasumi <redgec1@gmail.com>
2021-11-13 19:47:14 +0100

    Adding and testing HTTP resources

--- src/main/java/com/manageserversproject/server/ServerApplication.java
+++ src/main/java/com/manageserversproject/server/ServerApplication.java
@@ -1,13 +1,32 @@
 package com.manageserversproject.server;
 
+import com.manageserversproject.server.enumeration.Status;
+import com.manageserversproject.server.model.Server;
+import com.manageserversproject.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.manageserversproject.server.enumeration.Status.SERVER_DOWN;
+import static com.manageserversproject.server.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo){
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.161", "Linux", "32 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.80", "Windows", "16 GB", "Web Server", "http://localhost:8080/server/image/server2.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.15", "Ubuntu", "64 GB", "Web Server", "http://localhost:8080/server/image/server3.png", SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.27", "Windows", "16 GB", "Personal PC", "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
+			serverRepo.save(new Server(null, "192.168.0.1", "Linux", "32 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+		};
 	}
 
 }
--- src/main/java/com/manageserversproject/server/service/ServerService.java
+++ src/main/java/com/manageserversproject/server/service/ServerService.java
@@ -1,2 +1,17 @@
-package com.manageserversproject.server.service;public interface ServerService {
+package com.manageserversproject.server.service;
+
+import com.manageserversproject.server.model.Server;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+
+public interface ServerService {
+    Server create(Server server);
+    Server ping(String ipAddress) throws IOException;
+    Collection<Server> list(int limit);
+    Server get(Long id);
+    Server update(Server server);
+    Boolean delete(Long id);
+
 }
